6471d6992053ebf68995a372bd88ffcb222e186a
ayush-ruhr <ayush.pandey@ruhr.agency>
2024-09-02 10:26:31 +0200

    correct app name in INSTALLED_APPS

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "translations.apps.TranslationsConfig",
+    "translation.apps.TranslationsConfig",
 ]
 
 MIDDLEWARE = [
