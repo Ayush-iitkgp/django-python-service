8177c1e501ff6bfe956cae7a2a513ccfbbbf7f4a
ayush-ruhr <ayush.pandey@ruhr.agency>
2024-09-04 14:52:58 +0200

    added translation to th admin panel

--- translation/admin.py
+++ translation/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from translation.models import Translation
+
+
+@admin.register(Translation)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = (
+        "translation_id",
+        "created_at",
+        "user_id",
+        "format",
+        "original_content",
+        "translated_content",
+    )
+    ordering = ("-created_at",)
