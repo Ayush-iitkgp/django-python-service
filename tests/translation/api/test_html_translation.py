1f8d5296d8d973f1d7ff2b90224064d2f1aa8622
ayush-iitkgp <ayushpandey.iitkgp@gmail.com>
2024-09-09 20:14:26 +0200

    add test for multiple html examples

--- tests/translation/api/test_html_translation.py
+++ tests/translation/api/test_html_translation.py
@@ -25,3 +25,18 @@ def test_translate_html_400_error(client_api_token: APIClient, request_html_payl
     request_html_payload.pop("format")
     response = client_api_token.post("/v1/translation/translate", data=request_html_payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.parametrize("filename", ["canvas_example"])
+def test_translate_multiple_html_success(
+    filename: str, client_api_token: APIClient, request_html_payload: dict, translated_html_content: str
+) -> None:
+    prefix = "tests/translation/api/data"
+    file_name = f"{prefix}/en/{filename}.html"
+    content = open(file_name, "rb").read()
+    request_html_payload["original_content"] = content
+    response = client_api_token.post(path="/v1/translation/translate", data=request_html_payload)
+    assert response.status_code == status.HTTP_201_CREATED
+    body = response.json()
+    assert "translated_content" in body
+    assert body["translated_content"] == translated_html_content
