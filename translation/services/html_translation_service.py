7f41d195955c8c562976514ac7f5778ae1c3541e
ayush-iitkgp <ayushpandey.iitkgp@gmail.com>
2024-09-10 01:01:59 +0200

    Improved sync translate method

--- translation/services/html_translation_service.py
+++ translation/services/html_translation_service.py
@@ -50,11 +50,6 @@ class HTMLTranslationService:
             return cls.translate_preserve_formatting(tag)
         elif tag.translateable is False:
             return tag
-        elif cls.depth(tag) == 2:
-            tag_injection = cls.inject_tags_inference(tag)
-            if tag_injection is not None:
-                logger.info("translate_tags", "tag injection successful")
-                return tag_injection
         else:
             tag.children = [cls.snyc_translate_tags(child) for child in tag.children]
 
@@ -123,88 +118,3 @@ class HTMLTranslationService:
         if len(tag.children) == 0:
             return 0
         return max([cls.depth(t) for t in tag.children])
-
-    @classmethod
-    def inject_tags_inference(cls, tag: Tag) -> Tag | None:
-        """Returns translated tag tree with injection tags, None if not possible
-
-        tag is only modified in place if tag injection is successful.
-
-        Args:
-            tag: A depth=2 tag tree to attempt injection on.
-
-        Returns:
-            A translated version of tag, None if not possible to tag inject
-        """
-        MAX_SEQUENCE_LENGTH = 200
-
-        text = tag.text()
-        if len(text) > MAX_SEQUENCE_LENGTH:
-            return None
-
-        translated_text = cls.translate_preserve_formatting(text)
-
-        class InjectionTag:
-            """
-
-            Attributes:
-                text: The text of the tag
-                tag: The depth 1 ITag it represents
-                injection_index: The index in the outer translated string that
-                        this tag can be injected into.
-            """
-
-            def __init__(self, text: str, tag: Tag):
-                self.text = text
-                self.tag = tag
-                self.injection_index = None
-
-        injection_tags = []
-        for child in tag.children:
-            if cls.depth(child) == 1:
-                translated = cls.translate_preserve_formatting(child.text())
-                injection_tags.append(InjectionTag(translated, child))
-            elif type(child) is not str:
-                logger.info("inject_tags_inference", "can't inject depth 0 ITag")
-                return None
-
-        for injection_tag in injection_tags:
-            injection_index = translated_text.find(injection_tag.text)
-            if injection_index != -1:
-                injection_tag.injection_index = injection_index
-            else:
-                logger.info(
-                    "inject_tags_inference",
-                    "injection text not found in translated text",
-                    translated_text,
-                    injection_tag.text,
-                )
-                return None
-
-        # Check for overlap
-        injection_tags.sort(key=lambda x: x.injection_index)
-        for i in range(len(injection_tags) - 1):
-            injection_tag = injection_tags[i]
-            next_injection_tag = injection_tags[i + 1]
-            if injection_tag.injection_index + len(injection_tag.text) >= next_injection_tag.injection_index:
-                logger.info(
-                    "inject_tags_inference",
-                    "injection tags overlap",
-                    injection_tag,
-                    next_injection_tag,
-                )
-                return None
-
-        to_return = []
-        i = 0
-        for injection_tag in injection_tags:
-            if i < injection_tag.injection_index:
-                to_return.append(translated_text[i : injection_tag.injection_index])
-            to_return.append(injection_tag.tag)
-            i = injection_tag.injection_index + len(injection_tag.text)
-        if i < len(translated_text):
-            to_return.append(translated_text[i:])
-
-        tag.children = to_return
-
-        return tag
