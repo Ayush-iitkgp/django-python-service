2991acce8c6d44dfbb0877795a4baee927337bf5
ayush-iitkgp <ayushpandey.iitkgp@gmail.com>
2024-09-09 18:14:22 +0200

    Fix test method names

--- tests/translation/api/test_html_translation.py
+++ tests/translation/api/test_html_translation.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIClient
 pytestmark = pytest.mark.django_db
 
 
-def test_translate_text_success(
+def test_translate_html_success(
     client_api_token: APIClient, request_html_payload: dict, translated_html_content: str
 ) -> None:
     response = client_api_token.post(path="/v1/translation/translate", data=request_html_payload)
@@ -15,13 +15,13 @@ def test_translate_text_success(
     assert body["translated_content"] == translated_html_content
 
 
-def test_translate_text_403_error(client_api_token: APIClient, request_html_payload: dict) -> None:
+def test_translate_html_403_error(client_api_token: APIClient, request_html_payload: dict) -> None:
     client_api_token.credentials(HTTP_AUTHORIZATION="Api-Key random key")
     response = client_api_token.post("/v1/translation/translate", data=request_html_payload)
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-def test_translate_text_400_error(client_api_token: APIClient, request_html_payload: dict) -> None:
+def test_translate_html_400_error(client_api_token: APIClient, request_html_payload: dict) -> None:
     request_html_payload.pop("format")
     response = client_api_token.post("/v1/translation/translate", data=request_html_payload)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
